8166baa8b2c5b30aa5ef956f5bedf5234972828f
David Conklin <dconklin09@gmail.com>
2016-12-16 16:06:44 -0500

    Changing to phantomjs-prebuilt npm package
    Removed call to phantomjs directly and replaced with phantomjs-prebuilt

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ var http = require('http');
 var theServer = require('./server.js');
 var server = http.createServer(theServer.handleRequest);
 var exec = require('child_process').exec;
+var phantom = require('phantomjs-prebuilt');
+var binPath = phantom.path;
 
 server.listen(8080);
 console.log('Server started on port 8080');
 
-var cmd = 'phantomjs capturesite.js http://localhost:8080';
+var cmd = binPath + ' capturesite.js http://localhost:8080';
 exec(cmd, function(error, sdout, sderr) {
     console.log(sdout);
     server.close(function() {
